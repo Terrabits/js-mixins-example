c3b7610bb7c3028e547322e0a0eb2f723baf9bc5
Nick Lalic <nick.lalic@gmail.com>
2019-11-28 22:14:18 -0800

    💡 improved legibility of Mixins class.

--- mixins/mixins.js
+++ mixins/mixins.js
@@ -1,9 +1,14 @@
 class Mixins {
   constructor(Mixins) {
     Mixins.forEach((Mixin) => {
+      // execute constructor
       Mixin['constructor'](this);
+
+      // copy everything except constructor
       const  properties = Object.assign({}, Mixin);
       delete properties['constructor'];
+
+      // add each to `this`
       for(const property in properties) {
         if (property === 'constructor') {
           continue;
